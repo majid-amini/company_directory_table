a1c8497fcff539f684ef5c7737a91d27c4d5b39d
unknown <majidamini_s@yahoo.com>
2022-09-06 15:44:31 +0430

    dom manipulation codes such as create html tags added to project

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ const nameInput = document.querySelector('.company_name input')
 const stateInput = document.querySelector('.company_state input')
 const employeesInput = document.querySelector('.company_employees input')
 const addButton = document.querySelector('.add_btn')
+const table = document.querySelector('tbody')
 
 
 addButton.addEventListener('click',(e) => {
     e.preventDefault()
-    
-})
+    table.innerHTML += `
+    <tr class="bg-white border-b">
+    <td class="p-1">${nameInput.value}</td>
+    <td>${stateInput.value}</td>
+    <td>${employeesInput.value}</td>
+    <td class="flex items-center gap-2">
+        <i class="fa-solid fa-pen-to-square"></i>
+        <i class="fa-solid fa-trash"></i>
+    </td>
+    </tr>
+    `})
