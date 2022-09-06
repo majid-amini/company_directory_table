4d25c8e7e7cb794c6507c7eaf364144f84b03bce
unknown <majidamini_s@yahoo.com>
2022-09-06 15:38:08 +0430

    js constants added for inputs

---
+++ src/App.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const nameInput = document.querySelector('.company_name input')
+const stateInput = document.querySelector('.company_state input')
+const employeesInput = document.querySelector('.company_employees input')
+const addButton = document.querySelector('.add_btn')
+
+
+addButton.addEventListener('click',(e) => {
+    e.preventDefault()
+    
+})
