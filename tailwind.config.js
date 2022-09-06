15a4c4733e58abb38076000958e9103069f51109
unknown <majidamini_s@yahoo.com>
2022-09-06 10:31:57 +0430

    total container and left side with form tag added to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns : {
+        
+        '13': '1fr 3fr',
+         
+      },
+    },
   },
   plugins: [],
 }
